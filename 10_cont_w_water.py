81ed8ed9b3322256cb6ed570ca2d77fac73bd889
BHAVYAPARMARR <bsparrmar21@gmail.com>
2025-08-09 00:31:22 -0400

    Create 10_cont_w_water.py

---
+++ 10_cont_w_water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        i, j = 0, len(height) - 1
+        best = 0
+
+        while i < j:
+            h = min(height[i], height[j])
+            best = max(best, h * (j - i))
+
+            # Move the shorter wall inward—only way to possibly get a taller min height
+            if height[i] < height[j]:
+                i += 1
+            else:
+                j -= 1
+
+        return best
