ecab029e247a9c2a22003fd4ff5c9fc119be607d
BHAVYAPARMARR <bsparrmar21@gmail.com>
2025-08-22 11:16:06 -0400

    Create 23_reverse_nodes_kgrp.py

---
+++ 23_reverse_nodes_kgrp.py
@@ -0,0 +1,37 @@
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head or k == 1:
+            return head
+
+        dummy = ListNode(0, head)
+        group_prev = dummy
+
+      
+        def get_kth(start: ListNode, k: int) -> Optional[ListNode]:
+            cur = start
+            for _ in range(k):
+                cur = cur.next
+                if not cur:
+                    return None
+            return cur
+
+        while True:
+            kth = get_kth(group_prev, k)
+            if not kth:
+                break
+            group_next = kth.next
+
+          
+            prev, cur = group_next, group_prev.next
+            while cur != group_next:
+                nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = nxt
+
+         
+            tmp = group_prev.next         # becomes the tail of this group
+            group_prev.next = kth         # new head of the group
+            group_prev = tmp              # move to next group
+
+        return dummy.next
